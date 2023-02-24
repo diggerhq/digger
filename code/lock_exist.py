cc372718562dbd00e7e31a34f4565e68f6e18020
motatoes <moe.habib9@gmail.com>
2023-02-24 18:33:21 +0000

    update code

--- code/lock_exist.py
+++ code/lock_exist.py
@@ -3,12 +3,8 @@ import boto3
 from simple_lock import get_lock
 
 # get a reference to the DynamoDB resource
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource("dynamodb")
 
 
-
-lock = get_lock(dynamodb, "resource", "tx-1")
+lock = get_lock(dynamodb, "resource")
 print(f"lock: {lock}")
-
-
-
