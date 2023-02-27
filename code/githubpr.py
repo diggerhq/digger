d14e3d91f8a48e6499034acc48e9075d3aef2a25
Mohamed Habib <moe.habib9@gmail.com>
2023-02-27 11:23:27 +0000

    do not swallow exception for create issue ATM

--- code/githubpr.py
+++ code/githubpr.py
@@ -94,7 +94,5 @@ class GitHubPR:
 
     def publish_comment(self, comment: str):
         """Add the given text as a comment in the pull request."""
-        try:
-            self.pull_request.create_issue_comment(comment)
-        except Exception:
-            print(f"ERROR: Failed to publish comment {comment}")
+        self.pull_request.create_issue_comment(comment)
+        
