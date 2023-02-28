087ec7e30ed1841f0ed6db53c0b9ca422388b12e
motatoes <moe.habib9@gmail.com>
2023-02-28 18:16:06 +0000

    less print, record event n action

--- code/usage.py
+++ code/usage.py
@@ -4,8 +4,8 @@ from hashlib import sha256
 
 def send_usage_record(repo_owner, event_name):
     payload = {
-        "userid": sha256(repo_owner.encode("utf-8")).hexdigest(), 
-        "action": event_name,
+        "userid": sha256(repo_owner.encode("utf-8")).hexdigest(),
+        "event_name": event_name,
         "token": "diggerABC@@1998fE"
     }
     url = "https://i2smwjphd4.execute-api.us-east-1.amazonaws.com/prod"
