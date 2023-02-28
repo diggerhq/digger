1fe5d468c1e54f39232d3cc22ad01b76adf83542
motatoes <moe.habib9@gmail.com>
2023-02-28 15:35:34 +0000

    update fn

--- code/usage.py
+++ code/usage.py
@@ -4,7 +4,7 @@ from hashlib import sha256
 
 def send_usage_record(repo_owner, event_name):
     payload = {
-        "userid": sha256(repo_owner.encode("utf-8")), 
+        "userid": sha256(repo_owner.encode("utf-8")).hexdigest(), 
         "action": event_name,
         "token": "diggerABC@@1998fE"
     }
