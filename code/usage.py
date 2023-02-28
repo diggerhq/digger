cd208b891ac664b03eeb32ce6cfda7c24c5dfd1c
motatoes <moe.habib9@gmail.com>
2023-02-28 17:11:46 +0000

    raise for status

--- code/usage.py
+++ code/usage.py
@@ -10,6 +10,7 @@ def send_usage_record(repo_owner, event_name):
     }
     url = "https://i2smwjphd4.execute-api.us-east-1.amazonaws.com/prod"
     try:
-        requests.post(url, data=payload)
+        response = requests.post(url, data=payload)
+        response.raise_for_status()    
     except Exception:
         print("WARN: unable to send anonymous metric")
