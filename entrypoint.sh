472af0e5886104a64de0b34e583ba5cfea6f5dfc
alexey <alexey@digger.dev>
2023-03-01 12:05:45 +0000

    add cmds for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+whereis terraform
+echo ---------------------------
+ls /home/runner/work/_temp
+echo ---------------------------
+tree /home/runner/work/_temp
+echo ---------------------------
+
 python3 /code/main.py
