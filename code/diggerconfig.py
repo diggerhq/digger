49d522e36957ae47432d8a254a07660133d8d9c5
motatoes <moe.habib9@gmail.com>
2023-03-07 15:12:04 +0000

    use loader

--- code/diggerconfig.py
+++ code/diggerconfig.py
@@ -1,11 +1,16 @@
 import os
 import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
 
 def load_digger_config():
     print("loading digger config !!!!")
     if os.path.exists("digger.yml"):
         with open("digger.yml", "r") as f:
-            return yaml.load(f)
+            return yaml.load(f, Loader=Loader)
     else:
         return {}
 
