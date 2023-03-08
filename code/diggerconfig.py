0a79b26d5f5f8cf25d4e4c229281bb81d582daac
motatoes <moe.habib9@gmail.com>
2023-03-08 15:59:18 +0000

    fix issues with get projects method

--- code/diggerconfig.py
+++ code/diggerconfig.py
@@ -13,17 +13,23 @@ class DiggerConfig():
         else:
             self.config = {}
 
+    def get_project(self, project_name):
+        for project in self.config["projects"]:
+            if project_name == project["name"]:
+                return project
+        return None
     def get_projects(self, project_name=None):
-        if self.config and self.config.get("projects") and project_name is None:
+        if self.config and self.config.get("projects") and not project_name:
             return self.config["projects"]
-        else:
-            return []
+        elif project_name:
+            project = self.get_project(project_name)
+            if project:
+                return project
+        return []
 
     def get_directory(self, project_name):
-        for project in  self.config["projects"]:
-            if project_name == project["name"]:
-                return project["dir"]
-        return None
+        project = self.get_project(project_name)
+        return project.get("dir", None)
 
 digger_config = DiggerConfig()
 
