71dae5540a533c1ac63f26d8827870b693920a38
alexey <alexey@digger.dev>
2023-03-10 14:17:03 +0000

    smart locking

--- code/digger_commands.py
+++ code/digger_commands.py
@@ -34,7 +34,7 @@ def lock_project(dynamodb, repo_name, pr_number, token, project_name=""):
     lock = get_lock(dynamodb, repo_name)
     pull_request = GitHubPR(repo_name, pr_number, token)
 
-    print(f"lock_project, lock:{lock}")
+    print(f"lock_project {project_name}, lock:{lock}")
     if lock:
         transaction_id = lock["transaction_id"]
         if int(pr_number) != int(transaction_id):
@@ -50,16 +50,16 @@ def lock_project(dynamodb, repo_name, pr_number, token, project_name=""):
 
     lock_acquired = acquire_lock(dynamodb, repo_name, 60 * 24, pr_number)
     if lock_acquired:
-        comment = f"Project has been locked by PR #{pr_number}"
+        comment = f"Project {project_name} has been locked by PR #{pr_number}"
         pull_request.publish_comment(comment)
-        print(f"project locked successfully. PR #{pr_number}")
+        print(f"project {project_name} locked successfully. PR #{pr_number}")
         # gha_utils.error("Run 'digger apply' to unlock the project.")
         # if for_terraform_run:
         #    # if we are going to run terraform we don't need to fail job
         return True
     else:
         lock = get_lock(dynamodb, repo_name)
-        comment = f"Project locked by another PR #{lock['transaction_id']} (failed to acquire lock {repo_name}). The locking plan must be applied or discarded before future plans can execute"
+        comment = f"Project {project_name} locked by another PR #{lock['transaction_id']} (failed to acquire lock {repo_name}). The locking plan must be applied or discarded before future plans can execute"
         pull_request.publish_comment(comment)
         print(comment)
         return False
