872f01d5bbe58b057e6873f81bea89727fbb8c1c
alexey <alexey@digger.dev>
2023-03-15 16:39:13 +0000

    add digger commands tests

--- tf.go
+++ tf.go
@@ -1,11 +1,11 @@
 package main
 
-type Terraform struct {
-}
-
 type TerraformExecutor interface {
 	Apply() error
 	Plan() error
+}
+
+type Terraform struct {
 }
 
 func (tf *Terraform) Apply() error {
@@ -15,5 +15,19 @@ func (tf *Terraform) Apply() error {
 
 func (tf *Terraform) Plan() error {
 	print("terraform plan")
+	return nil
+}
+
+type MockTerraform struct {
+	commands []string
+}
+
+func (tf *MockTerraform) Apply() error {
+	tf.commands = append(tf.commands, "apply")
+	return nil
+}
+
+func (tf *MockTerraform) Plan() error {
+	tf.commands = append(tf.commands, "plan")
 	return nil
 }
