e2c3ecdabab2ad97ab11a07f4e2b80d0cca4c97b
motatoes <moe.habib9@gmail.com>
2023-03-16 18:29:36 +0000

    fix tests

--- dynamo_locking_test.go
+++ dynamo_locking_test.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDynamoDbTableLockAndUnlock(t *testing.T) {
+	mockDynamoDB := MockDynamoDb{make(map[string]int)}
+	mockDynamoDB.Lock(1, 1, "mylock")
+	lock, err := mockDynamoDB.GetLock("mylock")
+	if err == nil {
+		assert.Equal(t, *lock, 1, "Expected lock ID to be 1")
+	} else {
+		t.Fatal("Error should not exist", err)
+	}
+}
+
+func TestDynamoDbTableLockExistsThrowsError(t *testing.T) {
+	mockDynamoDB := MockDynamoDb{make(map[string]int)}
+	_, err := mockDynamoDB.Lock(1, 1, "mylock")
+	assert.NoError(t, err)
+	_, err2 := mockDynamoDB.Lock(1, 1, "mylock")
+	assert.Error(t, err2)
+}
+
+func TestLockingTwiceThrowsError(t *testing.T) {
+	mockDynamoDB := MockDynamoDb{make(map[string]int)}
+	mockPrManager := MockGithubPullrequestManager{}
+	pl := ProjectLockImpl{
+		InternalLock: &mockDynamoDB,
+		PrManager:    &mockPrManager,
+		ProjectName:  "",
+		RepoName:     "",
+	}
+	state1, err1 := pl.Lock("a", 1)
+	assert.True(t, state1)
+	assert.NoError(t, err1)
+	state2, err2 := pl.Lock("a", 2)
+	assert.False(t, state2)
+	assert.Error(t, err2)
+}
