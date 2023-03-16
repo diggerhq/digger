e3b05016b28f76d372d59bf2e128d85e1952adec
alexey <alexey@digger.dev>
2023-03-16 11:21:00 +0000

    move mocks to mocks.go

--- tf.go
+++ tf.go
@@ -17,17 +17,3 @@ func (tf *Terraform) Plan() error {
 	print("terraform plan")
 	return nil
 }
-
-type MockTerraform struct {
-	commands []string
-}
-
-func (tf *MockTerraform) Apply() error {
-	tf.commands = append(tf.commands, "apply")
-	return nil
-}
-
-func (tf *MockTerraform) Plan() error {
-	tf.commands = append(tf.commands, "plan")
-	return nil
-}
