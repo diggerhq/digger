c5a3cdeef95f64e1b89d7b3bda464ab3c1a97d5b
motatoes <moe.habib9@gmail.com>
2023-03-20 11:13:08 +0000

    add usage test

--- usage_test.go
+++ usage_test.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 // util function for testing of send usage record
-func SendingUsageRecordTest(t *testing.T) {
+func TestSendingUsageRecord(t *testing.T) {
+	if os.Getenv("MANUAL_TEST") != "" {
+		t.Skip("Skipping not finished test")
+	}
 	err := sendUsageRecord("repoOwner", "testEvent", "testing")
 	assert.Nil(t, err)
 }
