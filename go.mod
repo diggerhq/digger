aa15eb0457a271d8a8d7c93eb8f8526a8491693c
alexey <alexey@digger.dev>
2023-03-22 12:48:56 +0000

    add google storage lock

--- go.mod
+++ go.mod
@@ -3,31 +3,47 @@ module digger
 go 1.18
 
 require (
+	cloud.google.com/go/storage v1.30.1
 	github.com/aws/aws-sdk-go v1.44.219
 	github.com/hashicorp/terraform-exec v0.18.1
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/stretchr/testify v1.8.2
-	golang.org/x/exp v0.0.0-20230315142452-642cacee5cc0
-	google.golang.org/appengine v1.6.7
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
+	cloud.google.com/go v0.110.0 // indirect
+	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v0.12.0 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
-	github.com/golang/protobuf v1.3.4 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.1 // indirect
 	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/hashicorp/terraform-json v0.15.0 // indirect
 	github.com/zclconf/go-cty v1.13.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/api v0.114.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230320184635-7606e756e683 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.29.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1
 	github.com/google/go-github/v50 v50.1.0
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
---
+++ pkg/gcp/gcp_lock.go
@@ -0,0 +1,116 @@
+package gcp
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type GoogleStorageLock struct {
+	Client *storage.Client
+	Bucket *storage.BucketHandle
+	ctx    context.Context
+}
+
+type Lock interface {
+	Lock(transactionId int, resource string) (bool, error)
+	Unlock(transactionId int, resource string) (bool, error)
+	GetLock(resource string) (*int, error)
+}
+
+func (googleLock *GoogleStorageLock) Lock(transactionId int, resource string) (bool, error) {
+	existingLockTransactionId, err := googleLock.GetLock(resource)
+	if err != nil {
+		fmt.Printf("failed to get lock: %v\n", err)
+	}
+	if existingLockTransactionId != nil {
+		return false, nil
+	}
+
+	now := time.Now().Format(time.RFC3339)
+	fileName := resource
+
+	wc := googleLock.Bucket.Object(fileName).NewWriter(googleLock.ctx)
+	wc.ContentType = "text/plain"
+	wc.Metadata = map[string]string{
+		"LockId":    strconv.Itoa(transactionId),
+		"CreatedAt": now,
+	}
+
+	bucketAttrs, err := googleLock.Bucket.Attrs(googleLock.ctx)
+	if err != nil {
+		fmt.Printf("failed to get bucket attributes: %v\n", err)
+	}
+	bucketName := bucketAttrs.Name
+
+	if err := wc.Close(); err != nil {
+		fmt.Printf("createFile: unable to close bucket %q, file %q: %v\n", bucketName, fileName, err)
+	}
+	return true, nil
+}
+
+func (googleLock *GoogleStorageLock) Unlock(transactionId int, resource string) (bool, error) {
+	fileName := resource
+
+	existingLockTransactionId, err := googleLock.GetLock(resource)
+	if err != nil {
+		fmt.Printf("failed to get lock: %v\n", err)
+	}
+	if existingLockTransactionId == nil {
+		return false, nil
+	}
+
+	if transactionId != *existingLockTransactionId {
+		fmt.Printf("failed to Unlock, transactionId doesn't match: %d != %d\n", transactionId, *existingLockTransactionId)
+		return false, nil
+	}
+
+	// lock exist, transactionId matches, we can delete the lock
+
+	bucketAttrs, err := googleLock.Bucket.Attrs(googleLock.ctx)
+	if err != nil {
+		fmt.Printf("failed to get bucket attributes: %v\n", err)
+	}
+	bucketName := bucketAttrs.Name
+	print("bucketname: ")
+	println(bucketName)
+
+	fileObject := googleLock.Bucket.Object(fileName)
+	err = fileObject.Delete(googleLock.ctx)
+	if err != nil {
+		return false, fmt.Errorf("failed to delete lock file: %v\n", err)
+	}
+
+	return true, nil
+}
+
+func (googleLock *GoogleStorageLock) GetLock(resource string) (*int, error) {
+	fileName := resource
+
+	bucketAttrs, err := googleLock.Bucket.Attrs(googleLock.ctx)
+	if err != nil {
+		fmt.Printf("failed to get bucket attributes: %v\n", err)
+	}
+	bucketName := bucketAttrs.Name
+	print("bucketname: ")
+	println(bucketName)
+
+	fileObject := googleLock.Bucket.Object(fileName)
+	fileAttrs, err := fileObject.Attrs(googleLock.ctx)
+	if err != nil {
+		// TODO: not sure if it's the best way to handle it
+		if err.Error() == "storage: object doesn't exist" {
+			return nil, nil
+		}
+		return nil, err
+	}
+	fileMetadata := fileAttrs.Metadata
+	lockIdStr := fileMetadata["LockId"]
+	transactionId, err := strconv.Atoi(lockIdStr)
+	if err != nil {
+		fmt.Printf("failed to parse LockId in object's metadata: %v\n", err)
+	}
+	return &transactionId, nil
+}
