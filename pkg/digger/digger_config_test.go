9ea33eb71bdda6f564fabe1674fa8e794e16c4d8
Youness Dendane <youness.dendane@fxinnovation.com>
2023-03-25 22:15:26 -0400

    Add tests for duplicate check

--- pkg/digger/digger_config_test.go
+++ pkg/digger/digger_config_test.go
@@ -4,14 +4,31 @@ import (
 	"log"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
-import "github.com/stretchr/testify/assert"
 
 func TestDiggerConfigFileDoesNotExist(t *testing.T) {
 	dg, err := NewDiggerConfig("")
 	assert.NoError(t, err, "expected error to be not nil")
 	assert.Equal(t, dg.Projects[0].Name, "default", "expected default project to have name 'default'")
 	assert.Equal(t, dg.Projects[0].Dir, ".", "expected default project dir to be '.'")
+}
+
+func TestMultipleDiggerConfigFileExist(t *testing.T) {
+	tempDir := CreateTempDir()
+	defer func(name string) {
+		err := os.RemoveAll(name)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(tempDir)
+	os.Create(tempDir + "digger.yaml")
+	os.Create(tempDir + "digger.yml")
+
+	dg, err := NewDiggerConfig(tempDir)
+	assert.Error(t, err, "expected error to be returned")
+	assert.Nil(t, dg, "expected diggerConfig to be nil")
 }
 
 func TestDefaultValuesForWorkflowConfiguration(t *testing.T) {
