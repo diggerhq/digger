6269471a48ac918ed2c54721be765bbade6b86ae
alexey <alexey@digger.dev>
2023-03-27 15:12:37 +0100

    update exit codes, add more logs

--- cmd/digger/main.go
+++ cmd/digger/main.go
@@ -5,6 +5,7 @@ import (
 	"digger/pkg/github"
 	"digger/pkg/models"
 	"digger/pkg/utils"
+	"fmt"
 	"os"
 	"strings"
 )
@@ -15,26 +16,33 @@ func main() {
 		println("Failed to read digger config.")
 		os.Exit(1)
 	}
+	println("Digger config read successfully")
 
 	lock, err := utils.GetLock()
 	if err != nil {
 		println("Failed to create lock.")
-		os.Exit(1)
+		os.Exit(2)
 	}
+	println("Lock has been created successfully")
 
 	ghToken := os.Getenv("GITHUB_TOKEN")
 	if ghToken == "" {
 		println("GITHUB_TOKEN is not defined")
-		os.Exit(1)
+		os.Exit(3)
 	}
 
 	ghContext := os.Getenv("GITHUB_CONTEXT")
 	if ghContext == "" {
 		println("GITHUB_CONTEXT is not defined")
-		os.Exit(1)
+		os.Exit(4)
 	}
 
 	parsedGhContext, err := models.GetGitHubContext(ghContext)
+	if err != nil {
+		println("Failed to parse GitHub context.")
+		os.Exit(5)
+	}
+	println("GitHub context parsed successfully")
 
 	ghEvent := parsedGhContext.Event
 	eventName := parsedGhContext.EventName
@@ -44,19 +52,22 @@ func main() {
 
 	impactedProjects, prNumber, err := digger.ProcessGitHubEvent(ghEvent, diggerConfig, githubPrService)
 	if err != nil {
-		println(err)
-		os.Exit(1)
+		fmt.Printf("failed to process GitHub event, %v", err)
+		os.Exit(6)
 	}
+	println("GitHub event processed successfully")
 
 	commandsToRunPerProject, err := digger.ConvertGithubEventToCommands(ghEvent, impactedProjects)
 	if err != nil {
-		println(err)
-		os.Exit(1)
+		fmt.Printf("failed to convert event to command, %v", err)
+		os.Exit(7)
 	}
+	println("GitHub event converted to commands successfully")
 
 	err = digger.RunCommandsPerProject(commandsToRunPerProject, repoOwner, repositoryName, eventName, prNumber, diggerConfig, githubPrService, lock, "")
 	if err != nil {
-		println(err)
-		os.Exit(1)
+		fmt.Printf("failed to execute command, %v", err)
+		os.Exit(8)
 	}
+	println("Commands executed successfully")
 }
