641ce30e0f77d88e14850de1f5d301ff3ee4d5df
Dias Saparov <dias@digger.dev>
2023-03-28 14:17:27 +0100

    Support workspaces (#72)
    * support workspaces

---
+++ pkg/digger/digger_test.go
@@ -0,0 +1,33 @@
+package digger
+
+import "testing"
+
+func TestParseWorkspace(t *testing.T) {
+	var commentTests = []struct {
+		in  string
+		out string
+		err bool
+	}{
+		{"test", "", false},
+		{"test -w workspace", "workspace", false},
+		{"test -w workspace -w workspace2", "", true},
+		{"test -w", "", true},
+	}
+
+	for _, tt := range commentTests {
+		out, err := parseWorkspace(tt.in)
+		if tt.err {
+			if err == nil {
+				t.Errorf("parseWorkspace(%q) = %q, want error", tt.in, out)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("parseWorkspace(%q) = %q, want %q", tt.in, err, tt.out)
+			}
+			if out != tt.out {
+				t.Errorf("parseWorkspace(%q) = %q, want %q", tt.in, out, tt.out)
+			}
+		}
+	}
+
+}
