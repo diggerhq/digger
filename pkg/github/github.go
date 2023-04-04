f29bd702478874a6f63fa469562cb4dc3d677ee3
s1ntaxe770r <cyberdev01@protonmail.com>
2023-04-04 15:29:58 +0100

    remove unused function

--- pkg/github/github.go
+++ pkg/github/github.go
@@ -25,7 +25,6 @@ type GithubPullRequestService struct {
 type PullRequestManager interface {
 	GetChangedFiles(prNumber int) ([]string, error)
 	PublishComment(prNumber int, comment string)
-	ReplyComment(prNumber int, reply string)
 }
 
 func (svc *GithubPullRequestService) GetChangedFiles(prNumber int) ([]string, error) {
@@ -46,18 +45,5 @@ func (svc *GithubPullRequestService) PublishComment(prNumber int, comment string
 	_, _, err := svc.Client.Issues.CreateComment(context.Background(), svc.Owner, svc.RepoName, prNumber, &github.IssueComment{Body: &comment})
 	if err != nil {
 		log.Fatalf("error publishing comment: %v", err)
-	}
-}
-
-func (svc *GithubPullRequestService) ReplyComment(prNumber int, reply string) {
-	comments, _, err := svc.Client.PullRequests.ListComments(context.Background(), svc.Owner, svc.RepoName, prNumber, nil)
-	if err != nil {
-		log.Fatalf("error getting pull request comments: %v", err)
-	}
-
-	for _, comment := range comments {
-		if comment.Body != nil && *comment.Body == "digger help" {
-			svc.Client.PullRequests.CreateCommentInReplyTo(context.Background(), svc.Owner, svc.RepoName, prNumber, reply, *comment.ID)
-		}
 	}
 }
