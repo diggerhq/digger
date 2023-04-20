2c810d179c020e93fa2c9786b72bfd4854bd02f7
alexey <alexey@digger.dev>
2023-04-20 09:38:21 +0100

    fix digger.yml path

--- pkg/digger/digger_config.go
+++ pkg/digger/digger_config.go
@@ -194,7 +194,7 @@ func NewDiggerConfig(workingDir string, walker DirWalker) (*DiggerConfig, error)
 		}
 		c, err := ConvertDiggerYamlToConfig(config, workingDir, walker)
 		if err != nil {
-			return nil, fmt.Errorf("failed to read config file: %v", err)
+			return nil, fmt.Errorf("failed to read config file %s: %v", fileName, err)
 		}
 		return c, nil
 	}
