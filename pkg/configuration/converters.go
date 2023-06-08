89b3e25e25c8382c44d6c79bd938d3ba40e30a2b
alexey <alexey@digger.dev>
2023-06-08 16:22:51 +0100

    merge develop branch

--- pkg/configuration/converters.go
+++ pkg/configuration/converters.go
@@ -107,6 +107,11 @@ func ConvertDiggerYamlToConfig(diggerYaml *DiggerConfigYaml, workingDir string, 
 		workflows := copyWorkflows(diggerYaml.Workflows)
 		diggerConfig.Workflows = workflows
 
+		// provide default workflow if not specified
+		if _, ok := diggerConfig.Workflows[defaultWorkflowName]; !ok {
+			workflow := *defaultWorkflow()
+			diggerConfig.Workflows[defaultWorkflowName] = workflow
+		}
 	} else {
 		workflow := *defaultWorkflow()
 		diggerConfig.Workflows = make(map[string]WorkflowConfig)
