a98f37d30fc80ab480aa3a9bce0d96f1002796ce
Dias Saparov <dias@digger.dev>
2023-06-12 11:40:32 +0100

    test

--- pkg/github/github.go
+++ pkg/github/github.go
@@ -148,6 +148,8 @@ func ConvertGithubEventToCommands(event models.Event, impactedProjects []configu
 
 			coreApplyStage := workflow.Apply.ToCoreStage()
 			corePlanStage := workflow.Plan.ToCoreStage()
+			fmt.Printf("Plan stage: %v\n", workflow.Plan)
+			fmt.Printf("Core plan stage: %v\n", corePlanStage)
 			if event.Action == "closed" && event.PullRequest.Merged && event.PullRequest.Base.Ref == event.Repository.DefaultBranch {
 				commandsPerProject = append(commandsPerProject, dg_models.ProjectCommand{
 					ProjectName:      project.Name,
