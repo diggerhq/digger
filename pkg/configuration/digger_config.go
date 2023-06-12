e66772068e1e76b6c5d91410f3158cdea217ef16
Dias Saparov <dias@digger.dev>
2023-06-12 11:31:13 +0100

    test

--- pkg/configuration/digger_config.go
+++ pkg/configuration/digger_config.go
@@ -233,6 +233,12 @@ func (s *Step) extract(stepMap map[string]interface{}, action string) {
 			}
 			s.ExtraArgs = extraArgs
 		}
+		if v, ok := stepMap[action].(map[string]interface{})["extra_args"]; ok {
+			for _, v := range v.([]interface{}) {
+				extraArgs = append(extraArgs, v.(string))
+			}
+			s.ExtraArgs = extraArgs
+		}
 	}
 }
 
