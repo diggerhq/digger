f45cbfa4e852bc1e07de61047c09dfd88b1cd5df
motatoes <moe.habib9@gmail.com>
2023-06-15 14:21:43 +0100

    fetch org first then project level policy

--- pkg/policy/policy.go
+++ pkg/policy/policy.go
@@ -2,15 +2,16 @@ package policy
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/open-policy-agent/opa/rego"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strings"
 )
 
 type PolicyProvider interface {
-	GetPolicy(namespace string, projectname string) (string, error)
+	GetPolicy(organisation string, namespace string, projectname string) (string, error)
 }
 
 type DiggerHttpPolicyProvider struct {
@@ -26,26 +27,73 @@ func (p NoOpPolicyChecker) Check(_ string, _ string, _ interface{}) (bool, error
 	return true, nil
 }
 
-func (p *DiggerHttpPolicyProvider) GetPolicy(namespace string, projectName string) (string, error) {
-	// fetch RBAC policies from Digger API
-	namespace = strings.ReplaceAll(namespace, "/", "-")
-	req, err := http.NewRequest("GET", p.DiggerHost+"/repos/"+namespace+"/projects/"+projectName+"/access-policy", nil)
+func (p *DiggerHttpPolicyProvider) getPolicyForOrganisation(organisation string, projectName string) (string, *http.Response, error) {
+
+	req, err := http.NewRequest("GET", p.DiggerHost+"/orgs/"+organisation+"/access-policy", nil)
 	if err != nil {
-		return "", err
+		return "", nil, err
 	}
 	req.Header.Add("Authorization", "Bearer "+p.AuthToken)
 
 	resp, err := p.HttpClient.Do(req)
 	if err != nil {
-		return "", err
+		return "", nil, err
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", resp, nil
+	}
+	return string(body), resp, nil
+}
+
+func (p *DiggerHttpPolicyProvider) getPolicyForNamespace(namespace string, projectName string) (string, *http.Response, error) {
+
+	// fetch RBAC policies for projectfrom Digger API
+	namespace = strings.ReplaceAll(namespace, "/", "-")
+	req, err := http.NewRequest("GET", p.DiggerHost+"/repos/"+namespace+"/projects/"+projectName+"/access-policy", nil)
+
+	if err != nil {
+		return "", nil, err
+	}
+	req.Header.Add("Authorization", "Bearer "+p.AuthToken)
+
+	resp, err := p.HttpClient.Do(req)
+	if err != nil {
+		return "", nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", resp, nil
+	}
+	return string(body), resp, nil
+
+}
+
+// GetPolicy fetches policy for entire organisation, if not found for organisation then it will fetch it for project
+func (p *DiggerHttpPolicyProvider) GetPolicy(organisation string, namespace string, projectName string) (string, error) {
+	content, resp, err := p.getPolicyForOrganisation(organisation, projectName)
 	if err != nil {
 		return "", err
 	}
-	return string(body), nil
+	if resp.StatusCode == 200 {
+		return content, nil
+	} else if resp.StatusCode == 404 {
+		content, resp, err := p.getPolicyForNamespace(namespace, projectName)
+		if err != nil {
+			return "", err
+		}
+		if resp.StatusCode == 200 {
+			return content, nil
+		} else {
+			return "", errors.New(fmt.Sprintf("unexpected response while fetching namespace policy: %v, code %v", content, resp.StatusCode))
+		}
+	} else {
+		return "", errors.New(fmt.Sprintf("unexpected response while fetching org policy: %v code %v", content, resp.StatusCode))
+	}
 }
 
 type DiggerPolicyChecker struct {
@@ -53,7 +101,7 @@ type DiggerPolicyChecker struct {
 }
 
 func (p DiggerPolicyChecker) Check(namespace string, projectName string, input interface{}) (bool, error) {
-	policy, err := p.PolicyProvider.GetPolicy(namespace, projectName)
+	policy, err := p.PolicyProvider.GetPolicy("", namespace, projectName)
 
 	if err != nil {
 		return false, err
