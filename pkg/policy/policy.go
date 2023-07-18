6eb9f18a4472441a6a5df6d078b3519d2ce3079a
Mohamed Habib <moe.habib9@gmail.com>
2023-07-18 17:39:06 +0300

    Fix for opa regressions (#454)
    * better parsing for hostname
    
    * improve readability and handle empty policy to fetch org

--- pkg/policy/policy.go
+++ pkg/policy/policy.go
@@ -7,7 +7,9 @@ import (
 	"fmt"
 	"github.com/open-policy-agent/opa/rego"
 	"io"
+	"log"
 	"net/http"
+	"net/url"
 )
 
 type PolicyProvider interface {
@@ -31,7 +33,12 @@ func (p NoOpPolicyChecker) Check(_ ci.CIService, _ string, _ string, _ string, _
 
 func getPolicyForOrganisation(p *DiggerHttpPolicyProvider) (string, *http.Response, error) {
 	organisation := p.DiggerOrganisation
-	req, err := http.NewRequest("GET", p.DiggerHost+"/orgs/"+organisation+"/access-policy", nil)
+	u, err := url.Parse(p.DiggerHost)
+	if err != nil {
+		log.Fatalf("Not able to parse digger cloud url: %v", err)
+	}
+	u.Path = "/orgs/" + organisation + "/access-policy"
+	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
 		return "", nil, err
 	}
@@ -52,7 +59,12 @@ func getPolicyForOrganisation(p *DiggerHttpPolicyProvider) (string, *http.Respon
 
 func getPolicyForNamespace(p *DiggerHttpPolicyProvider, namespace string, projectName string) (string, *http.Response, error) {
 	// fetch RBAC policies for project from Digger API
-	req, err := http.NewRequest("GET", p.DiggerHost+"/repos/"+namespace+"/projects/"+projectName+"/access-policy", nil)
+	u, err := url.Parse(p.DiggerHost)
+	if err != nil {
+		log.Fatalf("Not able to parse digger cloud url: %v", err)
+	}
+	u.Path = "/repos/" + namespace + "/projects/" + projectName + "/access-policy"
+	req, err := http.NewRequest("GET", u.String(), nil)
 
 	if err != nil {
 		return "", nil, err
@@ -81,9 +93,13 @@ func (p *DiggerHttpPolicyProvider) GetPolicy(organisation string, repo string, p
 		return "", err
 	}
 
+	// project policy found
 	if resp.StatusCode == 200 && content != "" {
 		return content, nil
-	} else if resp.StatusCode == 404 {
+	}
+
+	// check if project policy was empty or not found (retrieve org policy if so)
+	if (resp.StatusCode == 200 && content == "") || resp.StatusCode == 404 {
 		content, resp, err := getPolicyForOrganisation(p)
 		if err != nil {
 			return "", err
@@ -96,7 +112,7 @@ func (p *DiggerHttpPolicyProvider) GetPolicy(organisation string, repo string, p
 			return "", errors.New(fmt.Sprintf("unexpected response while fetching organisation policy: %v, code %v", content, resp.StatusCode))
 		}
 	} else {
-		return "", errors.New(fmt.Sprintf("unexpected response while fetching org policy: %v code %v", content, resp.StatusCode))
+		return "", errors.New(fmt.Sprintf("unexpected response while fetching project policy: %v code %v", content, resp.StatusCode))
 	}
 }
 
