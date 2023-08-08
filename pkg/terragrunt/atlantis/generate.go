53b53e8cfac365d0e8ba98132abf5f8eb5f28eb2
Dias Saparov <dias@digger.dev>
2023-08-08 15:29:27 +0100

    atlantis to digger auto generation

--- pkg/terragrunt/atlantis/generate.go
+++ pkg/terragrunt/atlantis/generate.go
@@ -358,7 +358,7 @@ func createProject(ignoreParentTerragrunt bool, ignoreDependencyBlocks bool, git
 		if !filepath.IsAbs(absolutePath) {
 			absolutePath = makePathAbsolute(gitRoot, dependencyPath, sourcePath)
 		}
-		absDependencies = append(absDependencies, absolutePath)
+		absDependencies = append(absDependencies, strings.TrimPrefix(absolutePath, gitRoot))
 		relativePath, err := filepath.Rel(absoluteSourceDir, absolutePath)
 		if err != nil {
 			return nil, err
