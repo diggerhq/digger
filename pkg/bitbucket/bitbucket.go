1f708fb0e5afbda24f8a309fa1df0985f975ff1a
Dias Saparov <dias@digger.dev>
2023-10-05 16:26:30 +0100

    test

--- pkg/bitbucket/bitbucket.go
+++ pkg/bitbucket/bitbucket.go
@@ -243,7 +243,7 @@ func (b *BitbucketAPI) SetStatus(prNumber int, status string, statusContext stri
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
 		body := &bytes.Buffer{}
 		_, err := body.ReadFrom(resp.Body)
 		if err != nil {
