884933761fd855c0ad3cda6dd6feba0034ff7623
motatoes <moe.habib9@gmail.com>
2023-11-07 19:22:13 +0000

    split large message into parts
    Former-commit-id: 45290cc8559ba7eb36dde257c4d5eaa544b36b18

--- pkg/notification/slack.go
+++ pkg/notification/slack.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 type Notification interface {
@@ -17,46 +18,66 @@ type SlackNotification struct {
 	Url string
 }
 
+func splitCodeBlocks(message string) []string {
+	var res []string
+	regex := regexp.MustCompile(`\\n`)
+	split := regex.Split(message, -1)
+	part := ""
+	for _, line := range split {
+		if len(part+line) > 4000 {
+			res = append(res, part)
+			part = line
+		} else {
+			part = part + line
+		}
+	}
+	return res
+}
+
 func (slack SlackNotification) Send(message string) error {
 	httpClient := &http.Client{}
 	type SlackMessage struct {
 		Text string `json:"text"`
 	}
-	slackMessage := SlackMessage{
-		Text: message,
-	}
+	parts := splitCodeBlocks(message)
+	for _, part := range (parts) {
+		slackMessage := SlackMessage{
+			Text: part,
+		}
 
-	jsonData, err := json.Marshal(slackMessage)
-	if err != nil {
-		msg := fmt.Sprintf("failed to marshal slack message. %v", err)
-		log.Printf(msg)
-		return fmt.Errorf(msg)
-	}
-
-	request, err := http.NewRequest("POST", slack.Url, bytes.NewBuffer(jsonData))
-	if err != nil {
-		msg := fmt.Sprintf("failed to create slack notification request. %v", err)
-		log.Printf(msg)
-		return fmt.Errorf(msg)
-	}
-
-	request.Header.Set("Content-Type", "application/json")
-	resp, err := httpClient.Do(request)
-	if err != nil {
-		msg := fmt.Sprintf("failed to send slack notification request. %v", err)
-		log.Printf(msg)
-	}
-	if resp.StatusCode != 200 {
-		body, err := io.ReadAll(resp.Body)
+		jsonData, err := json.Marshal(slackMessage)
 		if err != nil {
-			msg := fmt.Sprintf("failed to read response body. %v", err)
+			msg := fmt.Sprintf("failed to marshal slack message. %v", err)
 			log.Printf(msg)
 			return fmt.Errorf(msg)
 		}
-		msg := fmt.Sprintf("failed to send slack notification request. %v. Message: %v", resp.Status, body)
-		log.Printf(msg)
-		return fmt.Errorf(msg)
+
+		request, err := http.NewRequest("POST", slack.Url, bytes.NewBuffer(jsonData))
+		if err != nil {
+			msg := fmt.Sprintf("failed to create slack notification request. %v", err)
+			log.Printf(msg)
+			return fmt.Errorf(msg)
+		}
+
+		request.Header.Set("Content-Type", "application/json")
+		resp, err := httpClient.Do(request)
+		if err != nil {
+			msg := fmt.Sprintf("failed to send slack notification request. %v", err)
+			log.Printf(msg)
+		}
+		if resp.StatusCode != 200 {
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				msg := fmt.Sprintf("failed to read response body. %v", err)
+				log.Printf(msg)
+				return fmt.Errorf(msg)
+			}
+			msg := fmt.Sprintf("failed to send slack notification request. %v. Message: %v", resp.Status, body)
+			log.Printf(msg)
+			return fmt.Errorf(msg)
+		}
+		defer resp.Body.Close()
 	}
-	defer resp.Body.Close()
+
 	return nil
 }
