02f7a398d9bba8a9910f7af6899181b1e7658b3d
motatoes <moe.habib9@gmail.com>
2023-11-10 19:15:03 +0000

    fix test
    Former-commit-id: 35550a61e0475f2ac81092a9ced59ab7473756f6

--- libs/digger_config/terragrunt/atlantis/generate.go
+++ libs/digger_config/terragrunt/atlantis/generate.go
@@ -255,11 +255,8 @@ func getDependencies(ignoreParentTerragrunt bool, ignoreDependencyBlocks bool, g
 			depPath := dep
 			terrOpts, _ := options.NewTerragruntOptionsWithConfigPath(depPath)
 			terrOpts.OriginalTerragruntConfigPath = terragruntOptions.OriginalTerragruntConfigPath
-			childDeps, skipProject, err := getDependencies(ignoreParentTerragrunt, ignoreDependencyBlocks, gitRoot, cascadeDependencies, depPath, terrOpts)
+			childDeps, _, err := getDependencies(ignoreParentTerragrunt, ignoreDependencyBlocks, gitRoot, cascadeDependencies, depPath, terrOpts)
 			if err != nil {
-				continue
-			}
-			if skipProject {
 				continue
 			}
 
