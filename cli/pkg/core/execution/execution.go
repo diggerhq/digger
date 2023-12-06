65865055c0e3eaf86e3a0ccfa37e6eb2fb6015f3
Igor Zalutski <izalutski@gmail.com>
2023-12-06 16:34:41 +0000

    Fail plan always to test build in GH actions; TODO revert
    Former-commit-id: 2721a0c6a8dffac3d7784075ab3e36aef0cba161

--- cli/pkg/core/execution/execution.go
+++ cli/pkg/core/execution/execution.go
@@ -126,6 +126,7 @@ func (d ProjectPathProvider) StoredPlanFilePath() string {
 }
 
 func (d DiggerExecutor) Plan() (bool, bool, string, string, error) {
+	return false, false, "", "", fmt.Errorf("error because let's see if it fails")
 	plan := ""
 	terraformPlanOutput := ""
 	isNonEmptyPlan := false
