f724eb733c5b3f64b4acac53308e5174927cf358
Mohamed Habib <moe.habib9@gmail.com>
2023-12-20 17:52:56 +0000

    set the access level for basic and http auth (#951)
    Former-commit-id: c83db17cbb524aa64d949ff5ee2ea68974c14a9b

--- backend/middleware/noop.go
+++ backend/middleware/noop.go
@@ -1,18 +1,22 @@
 package middleware
 
 import (
+	"github.com/diggerhq/digger/backend/models"
 	"github.com/gin-gonic/gin"
 )
 
 func NoopWebAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Set(ORGANISATION_ID_KEY, 1)
+		setDefaultOrganisationId(c)
+		c.Set(ACCESS_LEVEL_KEY, models.AdminPolicyType)
 		c.Next()
 	}
 }
 
 func NoopApiAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		setDefaultOrganisationId(c)
+		c.Set(ACCESS_LEVEL_KEY, models.AdminPolicyType)
 		c.Next()
 	}
 }
