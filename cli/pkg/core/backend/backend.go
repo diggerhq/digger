47b3e5fa3b1b85826d3aaa671dec045c84338268
motatoes <moe.habib9@gmail.com>
2024-01-15 16:47:33 +0000

    cli to report resources updated back to backend

--- cli/pkg/core/backend/backend.go
+++ cli/pkg/core/backend/backend.go
@@ -2,8 +2,25 @@ package backend
 
 import "time"
 
+type JobSummary struct {
+	ResourcesCreated uint
+	ResourcesUpdated uint
+	ResourcesDeleted uint
+}
+
 type Api interface {
 	ReportProject(repo string, projectName string, configuration string) error
 	ReportProjectRun(repo string, projectName string, startedAt time.Time, endedAt time.Time, status string, command string, output string) error
-	ReportProjectJobStatus(repo string, projectName string, jobId string, status string, timestamp time.Time) error
+	ReportProjectJobStatus(repo string, projectName string, jobId string, status string, timestamp time.Time, summary *JobSummary) error
+}
+
+func (j *JobSummary) ToJson() map[string]interface{} {
+	if j == nil {
+		return map[string]interface{}{}
+	}
+	return map[string]interface{}{
+		"resources_created": j.ResourcesCreated,
+		"resources_updated": j.ResourcesUpdated,
+		"resources_deleted": j.ResourcesDeleted,
+	}
 }
