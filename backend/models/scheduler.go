75542b04f8354ec77e506aefc683b4eedc093c8b
motatoes <moe.habib9@gmail.com>
2024-01-15 16:25:18 +0000

    backend reportStatus method update

--- backend/models/scheduler.go
+++ backend/models/scheduler.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
@@ -54,12 +55,21 @@ type DiggerBatch struct {
 
 type DiggerJob struct {
 	gorm.Model
-	DiggerJobId     string `gorm:"size:50,index:idx_digger_job_id"`
-	Status          DiggerJobStatus
-	Batch           *DiggerBatch
-	BatchID         *string `gorm:"index:idx_digger_job_id"`
-	SerializedJob   []byte
-	StatusUpdatedAt time.Time
+	DiggerJobId        string `gorm:"size:50,index:idx_digger_job_id"`
+	Status             DiggerJobStatus
+	Batch              *DiggerBatch
+	BatchID            *string `gorm:"index:idx_digger_job_id"`
+	DiggerJobSummary   *DiggerJobSummary
+	DiggerJobSummaryId *uint `gorm:""`
+	SerializedJob      []byte
+	StatusUpdatedAt    time.Time
+}
+
+type DiggerJobSummary struct {
+	gorm.Model
+	ResourcesCreated uint
+	ResourcesDeleted uint
+	ResourcesUpdated uint
 }
 
 type DiggerJobLinkStatus int8
@@ -77,4 +87,66 @@ type GithubDiggerJobLink struct {
 	GithubJobId         int64 `gorm:"index:idx_github_job_id"`
 	GithubWorkflowRunId int64
 	Status              DiggerJobLinkStatus
+}
+
+type SerializedJob struct {
+	DiggerJobId      string
+	Status           DiggerJobStatus
+	ProjectName      string
+	ResourcesCreated uint
+	ResourcesDeleted uint
+	resourcesUpdated uint
+}
+
+type SerializedBatch struct {
+	ID           string
+	PrNumber     int
+	Status       DiggerBatchStatus
+	BranchName   string
+	RepoFullName string
+	RepoOwner    string
+	RepoName     string
+	BatchType    DiggerBatchType
+	Jobs         []SerializedJob
+}
+
+func (j *DiggerJob) MapToJsonStruct() interface{} {
+	if j.DiggerJobSummary == nil {
+		return SerializedJob{
+			DiggerJobId: j.DiggerJobId,
+			Status:      j.Status,
+		}
+	} else {
+		return SerializedJob{
+			DiggerJobId:      j.DiggerJobId,
+			Status:           j.Status,
+			ResourcesCreated: j.DiggerJobSummary.ResourcesCreated,
+			resourcesUpdated: j.DiggerJobSummary.ResourcesUpdated,
+			ResourcesDeleted: j.DiggerJobSummary.ResourcesDeleted,
+		}
+	}
+}
+func (b *DiggerBatch) MapToJsonStruct() (interface{}, error) {
+
+	res := SerializedBatch{
+		ID:           b.ID.String(),
+		PrNumber:     b.PrNumber,
+		Status:       b.Status,
+		BranchName:   b.BranchName,
+		RepoFullName: b.RepoFullName,
+		RepoOwner:    b.RepoOwner,
+		RepoName:     b.RepoName,
+		BatchType:    b.BatchType,
+	}
+
+	serializedJobs := make([]SerializedJob, 0)
+	jobs, err := DB.GetDiggerJobsForBatch(b.ID)
+	if err != nil {
+		return nil, fmt.Errorf("Could not unmarshall digger batch: %v", err)
+	}
+	for _, job := range jobs {
+		serializedJobs = append(serializedJobs, job.MapToJsonStruct().(SerializedJob))
+	}
+	res.Jobs = serializedJobs
+	return res, nil
 }
