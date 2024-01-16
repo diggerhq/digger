11e4ef96a2319746cac6e6a28c627deac7e86e30
motatoes <moe.habib9@gmail.com>
2024-01-16 10:34:55 +0000

    implement publish comment method

--- backend/utils/github.go
+++ backend/utils/github.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/diggerhq/digger/backend/models"
+	"github.com/diggerhq/digger/libs/orchestrator"
 	github2 "github.com/diggerhq/digger/libs/orchestrator/github"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
@@ -114,4 +115,36 @@ func GetGithubService(gh GithubClientProvider, installationId int64, repoFullNam
 	}
 
 	return &ghService, token, nil
+}
+
+func SetPRStatusForJobs(prService *github2.GithubService, prNumber int, jobs []orchestrator.Job) error {
+	for _, job := range jobs {
+		for _, command := range job.Commands {
+			var err error
+			switch command {
+			case "digger plan":
+				err = prService.SetStatus(prNumber, "pending", job.ProjectName+"/plan")
+			case "digger apply":
+				err = prService.SetStatus(prNumber, "pending", job.ProjectName+"/apply")
+			}
+			if err != nil {
+				log.Printf("Erorr setting status: %v", err)
+				return fmt.Errorf("Error setting pr status: %v", err)
+			}
+		}
+	}
+	return nil
+}
+
+func AddInitialCommentJobs(prService *github2.GithubService, prNumber int, jobs []orchestrator.Job) (int, error) {
+	message := ":arrow_right: The following projects are impacted\n\n"
+	for _, job := range jobs {
+		message = message + fmt.Sprintf(""+
+			"<!-- PROJECTHOLDER %v -->\n"+
+			":airplane: %v Pending\n"+
+			"<!-- PROJECTHOLDEREND %v -->\n"+
+			"", job.ProjectName, job.ProjectName, job.ProjectName)
+	}
+	prService.PublishComment(prNumber, message)
+	return -1, nil
 }
