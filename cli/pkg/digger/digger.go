b5ce9d6c87ec8f38ef3dc615537dfdf999c8c30f
motatoes <moe.habib9@gmail.com>
2024-02-09 16:33:38 +0000

    removal of additional print debug statements

--- cli/pkg/digger/digger.go
+++ cli/pkg/digger/digger.go
@@ -3,7 +3,6 @@ package digger
 import (
 	"errors"
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"log"
 	"os"
 	"path"
@@ -170,10 +169,6 @@ func UpdateStatusComment(jobs []scheduler.SerializedJob, prNumber int, prService
 			log.Printf("Failed to convert unmarshall Serialized job")
 		}
 		isPlan := jobSpec.IsPlan()
-
-		fmt.Printf("job details: %v | %v\n", jobSpec.Commands, isPlan)
-		spew.Dump(jobSpec.Commands)
-		fmt.Printf("%v ---- %v", jobSpec.Commands[0], jobSpec.Commands[0] == "digger plan")
 
 		message = message + fmt.Sprintf("<!-- PROJECTHOLDER %v -->\n", job.ProjectName)
 		message = message + fmt.Sprintf("%v **%v** <a href='%v'>%v</a>%v\n", job.Status.ToEmoji(), jobSpec.ProjectName, *job.WorkflowRunUrl, job.Status.ToString(), job.ResourcesSummaryString(isPlan))
