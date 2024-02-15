17ccdb9342e212d8117b1fd333033a6226248c1a
motatoes <moe.habib9@gmail.com>
2024-02-15 17:02:20 +0000

    add filename

--- cli/pkg/core/storage/plan_storage.go
+++ cli/pkg/core/storage/plan_storage.go
@@ -2,7 +2,7 @@ package storage
 
 type PlanStorage interface {
 	StorePlan(localPlanFilePath string, storedPlanFilePath string) error
-	StorePlanFile(fileContents []byte, artifactName string) error
+	StorePlanFile(fileContents []byte, artifactName string, fileName string) error
 	RetrievePlan(localPlanFilePath string, storedPlanFilePath string) (*string, error)
 	DeleteStoredPlan(storedPlanFilePath string) error
 	PlanExists(storedPlanFilePath string) (bool, error)
