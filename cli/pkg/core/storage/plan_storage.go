eb173427c3fb79fc161906f0742e60dcdfab761e
motatoes <moe.habib9@gmail.com>
2024-02-15 15:45:07 +0000

    testing of upload single file

--- cli/pkg/core/storage/plan_storage.go
+++ cli/pkg/core/storage/plan_storage.go
@@ -2,6 +2,7 @@ package storage
 
 type PlanStorage interface {
 	StorePlan(localPlanFilePath string, storedPlanFilePath string) error
+	StorePlanFile(fileContents []byte, artifactName string) error
 	RetrievePlan(localPlanFilePath string, storedPlanFilePath string) (*string, error)
 	DeleteStoredPlan(storedPlanFilePath string) error
 	PlanExists(storedPlanFilePath string) (bool, error)
