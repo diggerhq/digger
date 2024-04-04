d0b77a71d6067c3e6de81231799041a633f8cd69
motatoes <moe.habib9@gmail.com>
2024-04-04 14:56:36 +0100

    fix build and make func create jobs for multiple projects

--- libs/orchestrator/utils.go
+++ libs/orchestrator/utils.go
@@ -1,34 +1,8 @@
 package orchestrator
 
 import (
-	"errors"
 	"regexp"
 )
-
-func ParseWorkspace(comment string) (string, error) {
-	re := regexp.MustCompile(`-w(?:\s+(\S+)|$)`)
-	matches := re.FindAllStringSubmatch(comment, -1)
-
-	if len(matches) == 0 {
-		return "", nil
-	}
-
-	if len(matches) > 1 {
-		return "", errors.New("more than one -w flag found")
-	}
-	workspaceOverride, err := orchestrator.ParseWorkspace(*payload.Comment.Body)
-	if err != nil {
-		return nil, err
-	}
-	if workspaceOverride != "" {
-		workspace = workspaceOverride
-	}
-	if len(matches[0]) < 2 || matches[0][1] == "" {
-		return "", errors.New("no value found after -w flag")
-	}
-
-	return matches[0][1], nil
-}
 
 func ParseProjectName(comment string) string {
 	re := regexp.MustCompile(`-p ([0-9a-zA-Z\-_]+)`)
