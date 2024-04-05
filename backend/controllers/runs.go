264011e31d294cd14f0a8d1edb93b00973d58df4
Mohamed Habib <moe.habib9@gmail.com>
2024-04-05 16:05:36 +0100

    Add run details endpoint (#1326)
    * make runs list nested
    
    * run details endpoint

--- backend/controllers/runs.go
+++ backend/controllers/runs.go
@@ -50,6 +50,13 @@ func RunsForProject(c *gin.Context) {
 		return
 	}
 
+	if project.OrganisationID != org.ID {
+		log.Printf("Forbidden access: not allowed to access projectID: %v logged in org: %v", project.OrganisationID, org.ID)
+		c.String(http.StatusForbidden, "No access to this project")
+		return
+
+	}
+
 	runs, err := models.DB.ListDiggerRunsForProject(project.Name, project.Repo.ID)
 	if err != nil {
 		log.Printf("could not fetch runs: %v", err)
@@ -67,5 +74,56 @@ func RunsForProject(c *gin.Context) {
 		}
 		serializedRuns = append(serializedRuns, serializedRun)
 	}
-	c.JSON(http.StatusOK, serializedRuns)
+	response := make(map[string]interface{})
+	response["runs"] = serializedRuns
+	c.JSON(http.StatusOK, response)
+}
+
+func RunDetails(c *gin.Context) {
+	currentOrg, exists := c.Get(middleware.ORGANISATION_ID_KEY)
+	runIdStr := c.Param("run_id")
+
+	if runIdStr == "" {
+		c.String(http.StatusBadRequest, "RunID not specified")
+		return
+	}
+
+	runId, err := strconv.Atoi(runIdStr)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Invalid RunId")
+		return
+	}
+
+	if !exists {
+		c.String(http.StatusForbidden, "Not allowed to access this resource")
+		return
+	}
+
+	var org models.Organisation
+	err = models.DB.GormDB.Where("id = ?", currentOrg).First(&org).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.String(http.StatusNotFound, fmt.Sprintf("Could not find organisation: %v", currentOrg))
+		} else {
+			c.String(http.StatusInternalServerError, "Unknown error occurred while fetching database")
+		}
+		return
+	}
+
+	run, err := models.DB.GetDiggerRun(uint(runId))
+	if err != nil {
+		log.Printf("Could not fetch run: %v", err)
+		c.String(http.StatusBadRequest, "Could not fetch run, please check that it exists")
+	}
+	if run.Repo.OrganisationID != org.ID {
+		c.String(http.StatusForbidden, "Not allowed to access this resource")
+		return
+	}
+
+	response, err := run.MapToJsonStruct()
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Could not unmarshall data")
+		return
+	}
+	c.JSON(http.StatusOK, response)
 }
