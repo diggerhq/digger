299298718d7f188b16fe618325467f1d2e5062a3
motatoes <moe.habib9@gmail.com>
2024-04-24 21:46:55 +0100

    project details endpoint

--- backend/controllers/projects.go
+++ backend/controllers/projects.go
@@ -13,6 +13,7 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -115,6 +116,54 @@ func FindProjectsForOrg(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, response)
+}
+
+func ProjectDetails(c *gin.Context) {
+
+	currentOrg, exists := c.Get(middleware.ORGANISATION_ID_KEY)
+	projectIdStr := c.Param("project_id")
+
+	if projectIdStr == "" {
+		c.String(http.StatusBadRequest, "ProjectId not specified")
+		return
+	}
+
+	projectId, err := strconv.Atoi(projectIdStr)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Invalid ProjectId")
+		return
+	}
+
+	if !exists {
+		c.String(http.StatusForbidden, "Not allowed to access this resource")
+		return
+	}
+
+	var org models.Organisation
+	err = models.DB.GormDB.Where("id = ?", currentOrg).First(&org).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.String(http.StatusNotFound, fmt.Sprintf("Could not find organisation: %v", currentOrg))
+		} else {
+			c.String(http.StatusInternalServerError, "Unknown error occurred while fetching database")
+		}
+		return
+	}
+
+	project, err := models.DB.GetProject(uint(projectId))
+	if err != nil {
+		log.Printf("could not fetch project: %v", err)
+		c.String(http.StatusInternalServerError, "Could not fetch project")
+		return
+	}
+
+	if project.OrganisationID != org.ID {
+		log.Printf("Forbidden access: not allowed to access projectID: %v logged in org: %v", project.OrganisationID, org.ID)
+		c.String(http.StatusForbidden, "No access to this project")
+		return
+	}
+
+	c.JSON(http.StatusOK, project.MapToJsonStruct())
 }
 
 type CreateProjectRequest struct {
