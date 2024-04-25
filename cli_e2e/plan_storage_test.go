0bc2457d1f69081e6be728626182547728f5e86b
Lars Haugan <456305+larhauga@users.noreply.github.com>
2024-04-25 14:23:19 +0200

    add e2e test for aws s3 plan storage (#1381)

--- cli_e2e/plan_storage_test.go
+++ cli_e2e/plan_storage_test.go
@@ -2,13 +2,14 @@ package cli_e2e
 
 import (
 	"fmt"
-	"github.com/diggerhq/digger/cli/pkg/gcp"
-	"github.com/diggerhq/digger/cli/pkg/storage"
-	"github.com/stretchr/testify/assert"
 	"log"
 	"os"
 	"strings"
 	"testing"
+
+	"github.com/diggerhq/digger/cli/pkg/gcp"
+	"github.com/diggerhq/digger/cli/pkg/storage"
+	"github.com/stretchr/testify/assert"
 )
 
 func init() {
@@ -17,7 +18,6 @@ func init() {
 }
 
 func TestGCPPlanStorageStorageAndRetrieval(t *testing.T) {
-	fmt.Printf("in function")
 	file, err := os.CreateTemp("/tmp", "prefix")
 	fmt.Printf("%v", err)
 	assert.Nil(t, err)
@@ -37,16 +37,52 @@ func TestGCPPlanStorageStorageAndRetrieval(t *testing.T) {
 	artefactName := "myartefact"
 	fileName := "myplan.tfplan"
 	err = planStorage.StorePlanFile(contents, artefactName, fileName)
-	fmt.Printf("%v", err)
+	fmt.Printf("error StorePlanFile: %v", err)
 	assert.Nil(t, err)
 	exists, err := planStorage.PlanExists(artefactName, fileName)
-	fmt.Printf("%v", err)
+	fmt.Printf("error PlanExists: %v", err)
 	assert.Nil(t, err)
 	assert.True(t, exists)
 
 	planStorage.RetrievePlan(file.Name(), artefactName, fileName)
 	readContents, err := os.ReadFile(file.Name())
-	fmt.Printf("%v", err)
+	fmt.Printf("error RetrievePlan: %v", err)
+	assert.Nil(t, err)
+	assert.Equal(t, readContents, contents)
+}
+
+func TestAWSPlanStorageStorageAndRetrieval(t *testing.T) {
+	file, err := os.CreateTemp("/tmp", "prefix")
+	fmt.Printf("error creating temp dir: %v", err)
+	assert.Nil(t, err)
+	defer os.Remove(file.Name())
+
+	fmt.Printf("getting AWS S3 client")
+	ctx, client, err := storage.GetAWSStorageClient()
+	if err != nil {
+		t.Errorf("failed to get AWS storage client: %v", err)
+	}
+	bucketName := strings.ToLower(os.Getenv("AWS_S3_BUCKET"))
+	fmt.Printf("using AWS S3 bucket found by env var 'AWS_S3_BUCKET': %s", bucketName)
+	planStorage := &storage.PlanStorageAWS{
+		Client:  client,
+		Bucket:  bucketName,
+		Context: ctx,
+	}
+	contents := []byte{'a'}
+	artefactName := "myartefact"
+	fileName := "myplan.tfplan"
+	err = planStorage.StorePlanFile(contents, artefactName, fileName)
+	fmt.Printf("error StorePlanFile: %v", err)
+	assert.Nil(t, err)
+	exists, err := planStorage.PlanExists(artefactName, fileName)
+	fmt.Printf("error PlanExists: %v", err)
+	assert.Nil(t, err)
+	assert.True(t, exists)
+
+	planStorage.RetrievePlan(file.Name(), artefactName, fileName)
+	readContents, err := os.ReadFile(file.Name())
+	fmt.Printf("error RetrievePlan: %v", err)
 	assert.Nil(t, err)
 	assert.Equal(t, readContents, contents)
 }
