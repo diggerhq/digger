0cb254c3576cb70c09e8ca8d2ac30a2562095a13
motatoes <moe.habib9@gmail.com>
2024-05-09 10:21:02 +0100

    segment close client

--- backend/controllers/github.go
+++ backend/controllers/github.go
@@ -506,7 +506,7 @@ func handlePullRequestEvent(gh utils.GithubClientProvider, payload *github.PullR
 		return fmt.Errorf("error converting jobs")
 	}
 
-	segment.Track(strconv.Itoa(int(organisationId)), "trigger_job")
+	segment.Track(strconv.Itoa(int(organisationId)), "backend_trigger_job")
 	err = TriggerDiggerJobs(ghService.Client, repoOwner, repoName, batchId, prNumber, ghService)
 	if err != nil {
 		log.Printf("TriggerDiggerJobs error: %v", err)
