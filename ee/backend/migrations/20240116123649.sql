263d4a21fbf8735285f61037cf63aa9f7e049c3f
Mohamed Habib <moe.habib9@gmail.com>
2024-05-20 16:28:38 +0100

    chore: prepare ee release for backend service (#1487)
    * temp commit
    
    * completion of docker release

--- ee/backend/migrations/20240116123649.sql
+++
@@ -1,2 +0,0 @@
--- Modify "digger_batches" table
-ALTER TABLE "public"."digger_batches" ADD COLUMN "comment_id" bigint NULL;
--- ee/backend/migrations/20240125181812.sql
+++
@@ -1,2 +0,0 @@
--- Modify "digger_jobs" table
-ALTER TABLE "public"."digger_jobs" ADD COLUMN "workflow_run_url" text NULL;
