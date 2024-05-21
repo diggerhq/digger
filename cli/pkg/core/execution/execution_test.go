a34d05999c35cf567aef2b60c7ca7dad690e0ff4
Ben Carter <bcarter@rvohealth.com>
2024-05-21 11:40:48 -0400

    fixed auto-format from vscode :(

--- cli/pkg/core/execution/execution_test.go
+++ cli/pkg/core/execution/execution_test.go
@@ -1,10 +1,10 @@
 package execution
 
 import (
+	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 )
 
 func TestCorrectCleanUpWithoutRegexDoesNotProduceException(t *testing.T) {
