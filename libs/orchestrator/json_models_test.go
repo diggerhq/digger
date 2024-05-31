084e528ab8efba0c7479730ba6a2970aa742ad93
Mohamed Habib <moe.habib9@gmail.com>
2024-05-31 12:07:40 +0100

    add tests to ensure fields in job and JobJson are matching (#1537)

--- libs/orchestrator/json_models_test.go
+++ libs/orchestrator/json_models_test.go
@@ -1,9 +1,64 @@
 package orchestrator
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"reflect"
+	"slices"
 	"testing"
 )
+
+func TestAllFieldsInJobJsonAreAlsoInJob(t *testing.T) {
+	spec := JobJson{}
+	job := Job{}
+
+	specVal := reflect.Indirect(reflect.ValueOf(spec))
+	nFieldsSpec := specVal.Type().NumField()
+
+	jobVal := reflect.Indirect(reflect.ValueOf(job))
+	nFieldsJob := jobVal.Type().NumField()
+	jobFileds := make([]string, 0)
+
+	for j := 0; j < nFieldsJob; j++ {
+		jobFileds = append(jobFileds, jobVal.Type().Field(j).Name)
+	}
+
+	fieldsToIgnore := []string{"JobType", "AwsRoleRegion", "StateRoleName", "CommandRoleName", "BackendHostname", "BackendOrganisationName", "BackendJobToken"}
+	for i := 0; i < nFieldsSpec; i++ {
+		field := specVal.Type().Field(i).Name
+		if slices.Contains(fieldsToIgnore, field) {
+			continue
+		}
+		assert.True(t, slices.Contains(jobFileds, field),
+			"IMPORTANT: Please ensure all fields are correctly passed in serialization")
+	}
+}
+
+func TestAllFieldsInJobAreAlsoInJobJson(t *testing.T) {
+	spec := JobJson{}
+	job := Job{}
+
+	jobVal := reflect.Indirect(reflect.ValueOf(job))
+	nFieldsJob := jobVal.Type().NumField()
+
+	specVal := reflect.Indirect(reflect.ValueOf(spec))
+	nFieldsSpec := specVal.Type().NumField()
+	specFields := make([]string, 0)
+	for j := 0; j < nFieldsSpec; j++ {
+		specFields = append(specFields, specVal.Type().Field(j).Name)
+	}
+
+	fmt.Printf("%v ::\n", specFields)
+	fieldsToIgnore := []string{"ProjectWorkflow", "StateEnvProvider", "CommandEnvProvider"}
+	for i := 0; i < nFieldsJob; i++ {
+		field := jobVal.Type().Field(i).Name
+		if slices.Contains(fieldsToIgnore, field) {
+			continue
+		}
+		assert.True(t, slices.Contains(specFields, field),
+			"IMPORTANT: Please ensure all fields are correctly passed in serialization")
+	}
+}
 
 func TestIsPlanForDiggerPlanJobCorrect(t *testing.T) {
 	j := JobJson{
