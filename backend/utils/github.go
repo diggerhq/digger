069a3aac7757dda1b888990ffea4a22150202357
Mohamed Habib <moe.habib9@gmail.com>
2024-06-03 16:17:10 +0100

    add additional inputs for dynamic names (#1543)
    * add additional inputs for dynamic run names

--- backend/utils/github.go
+++ backend/utils/github.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/diggerhq/digger/backend/models"
@@ -222,12 +223,27 @@ func GetWorkflowIdAndUrlFromDiggerJobId(client *github.Client, repoOwner string,
 
 func TriggerGithubWorkflow(client *github.Client, repoOwner string, repoName string, job models.DiggerJob, jobString string, commentId int64) error {
 	log.Printf("TriggerGithubWorkflow: repoOwner: %v, repoName: %v, commentId: %v", repoOwner, repoName, commentId)
+
+	var jobSpec orchestrator.JobJson
+	err := json.Unmarshal([]byte(jobString), &jobSpec)
+	if err != nil {
+		log.Printf("could not unmarshal job string: %v", err)
+		return fmt.Errorf("could not marshal json string: %v", err)
+	}
+
+	batchIdShort := job.Batch.ID.String()[:8]
+	diggerCommand := fmt.Sprintf("digger %v", job.Batch.BatchType)
+	projectName := jobSpec.ProjectName
+	requestedBy := jobSpec.RequestedBy
+	prNumber := *jobSpec.PullRequestNumber
 	inputs := orchestrator_scheduler.WorkflowInput{
 		Id:        job.DiggerJobID,
 		JobString: jobString,
 		CommentId: strconv.FormatInt(commentId, 10),
+		RunName:   fmt.Sprintf("[%v] %v %v By: %v PR: %v", batchIdShort, diggerCommand, projectName, requestedBy, prNumber),
 	}
-	_, err := client.Actions.CreateWorkflowDispatchEventByFileName(context.Background(), repoOwner, repoName, job.WorkflowFile, github.CreateWorkflowDispatchEventRequest{
+
+	_, err = client.Actions.CreateWorkflowDispatchEventByFileName(context.Background(), repoOwner, repoName, job.WorkflowFile, github.CreateWorkflowDispatchEventRequest{
 		Ref:    job.Batch.BranchName,
 		Inputs: inputs.ToMap(),
 	})
