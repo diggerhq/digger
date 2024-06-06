cfc31e21b417c3cb0646440348cdf0b425ddb916
motatoes <moe.habib9@gmail.com>
2024-06-06 10:47:41 +0100

    make fields public

--- libs/spec/models.go
+++ libs/spec/models.go
@@ -2,27 +2,16 @@ package spec
 
 import "github.com/diggerhq/digger/libs/orchestrator"
 
-type StepJson struct {
-	Action    string   `json:"action"`
-	Value     string   `json:"value"`
-	ExtraArgs []string `json:"extraArgs"`
-	Shell     string   `json:"shell"`
-}
-
-type StageJson struct {
-	Steps []StepJson `json:"steps"`
-}
-
 type ReporterSpec struct {
-	reporting_strategy string `json:"reporting_strategy"`
+	Reporting_strategy string `json:"reporting_strategy"`
 }
 
 type LockSpec struct {
-	lockType string `json:"lock_type"`
+	LockType string `json:"lock_type"`
 }
 
 type BackendSpec struct {
-	backendType             string `json:"backend_type"`
+	BackendType             string `json:"backend_type"`
 	BackendHostname         string `json:"backend_hostname"`
 	BackendOrganisationName string `json:"backend_organisation_hostname"`
 	BackendJobToken         string `json:"backend_job_token"`
