3496e66ec5591dce1c4b139323f4a9c39a2c53c0
Mohamed Habib <moe.habib9@gmail.com>
2024-06-09 13:02:54 +0100

    buildkite spec (#1572)
    * [ee] add buildkite support in orchestrator

--- ee/backend/ci_backends/buildkite.go
+++ ee/backend/ci_backends/buildkite.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"github.com/buildkite/go-buildkite/v3/buildkite"
 	"github.com/diggerhq/digger/backend/models"
+	"github.com/diggerhq/digger/backend/utils"
 	"github.com/diggerhq/digger/libs/orchestrator"
-	orchestrator_scheduler "github.com/diggerhq/digger/libs/orchestrator/scheduler"
+	"github.com/diggerhq/digger/libs/spec"
 	"log"
 	"strconv"
 )
@@ -34,18 +35,59 @@ func (b BuildkiteCi) TriggerWorkflow(repoOwner string, repoName string, job mode
 	branch := jobSpec.Branch
 	commitSha := jobSpec.Commit
 
-	inputs := orchestrator_scheduler.WorkflowInput{
-		Id:        job.DiggerJobID,
-		JobString: jobString,
+	runName := fmt.Sprintf("[%v] %v %v By: %v PR: %v", batchIdShort, diggerCommand, projectName, requestedBy, prNumber)
+	spec := spec.Spec{
+		JobId:     job.DiggerJobID,
 		CommentId: strconv.FormatInt(commentId, 10),
-		RunName:   fmt.Sprintf("[%v] %v %v By: %v PR: %v", batchIdShort, diggerCommand, projectName, requestedBy, prNumber),
+		RunName:   runName,
+		Job:       jobSpec,
+		Reporter: spec.ReporterSpec{
+			ReportingStrategy: "comments_per_run",
+			ReporterType:      "lazy",
+		},
+		Lock: spec.LockSpec{
+			LockType: "noop",
+		},
+		Backend: spec.BackendSpec{
+			BackendHostname:         jobSpec.BackendHostname,
+			BackendOrganisationName: jobSpec.BackendOrganisationName,
+			BackendJobToken:         jobSpec.BackendJobToken,
+			BackendType:             "backend",
+		},
+		VCS: spec.VcsSpec{
+			VcsType:   "github",
+			Actor:     jobSpec.RequestedBy,
+			RepoOwner: repoOwner,
+			RepoName:  repoName,
+		},
+		Policy: spec.PolicySpec{
+			PolicyType: "http",
+		},
 	}
 
+	_, ghToken, err := utils.GetGithubService(
+		utils.DiggerGithubRealClientProvider{},
+		job.Batch.GithubInstallationId,
+		job.Batch.RepoFullName,
+		job.Batch.RepoOwner,
+		job.Batch.RepoName,
+	)
+	if err != nil {
+		return fmt.Errorf("TriggerWorkflow: could not retrieve token: %v", err)
+	}
+
+	specBytes, err := json.Marshal(spec)
 	client := b.Client
 	_, _, err = client.Builds.Create(b.Org, b.Pipeline, &buildkite.CreateBuild{
 		Commit:  commitSha,
 		Branch:  branch,
-		Message: inputs.RunName,
+		Message: runName,
+		Author:  buildkite.Author{Username: requestedBy},
+		Env: map[string]string{
+			"DIGGER_SPEC":  string(specBytes),
+			"GITHUB_TOKEN": *ghToken,
+		},
+		PullRequestID: int64(prNumber),
 	})
 
 	return err
