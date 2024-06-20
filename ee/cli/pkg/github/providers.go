9e68e6e0c0b85c6a0d68ac0e42e38b7eb2943444
motatoes <moe.habib9@gmail.com>
2024-06-20 15:45:53 +0100

    fix the error calling cli

--- ee/cli/pkg/github/providers.go
+++ ee/cli/pkg/github/providers.go
@@ -10,23 +10,28 @@ import (
 
 type GithubServiceProviderAdvanced struct{}
 
-func (_ GithubServiceProviderAdvanced) NewService(ghToken string, repoName string, owner string) dg_github.GithubService {
+func (_ GithubServiceProviderAdvanced) NewService(ghToken string, repoName string, owner string) (dg_github.GithubService, error) {
 	client := github.NewClient(nil)
 	if ghToken != "" {
 		client = client.WithAuthToken(ghToken)
 	}
 
 	githubHostname := os.Getenv("DIGGER_GITHUB_HOSTNAME")
+	var err error
 	if githubHostname != "" {
 		log.Printf("info: using github hostname: %v", githubHostname)
 		githubEnterpriseBaseUrl := fmt.Sprintf("https://%v/api/v3/", githubHostname)
 		githubEnterpriseUploadUrl := fmt.Sprintf("https://%v/api/uploads/", githubHostname)
-		client.WithEnterpriseURLs(githubEnterpriseBaseUrl, githubEnterpriseUploadUrl)
+		client, err = client.WithEnterpriseURLs(githubEnterpriseBaseUrl, githubEnterpriseUploadUrl)
+		if err != nil {
+			log.Printf("error: could not create enterprise client: %v", err)
+			return dg_github.GithubService{}, fmt.Errorf("could not create enterprise client: %v", err)
+		}
 	}
 
 	return dg_github.GithubService{
 		Client:   client,
 		RepoName: repoName,
 		Owner:    owner,
-	}
+	}, nil
 }
