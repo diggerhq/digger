f58d5d6aa75b62f5198a37d442d16e064366528e
motatoes <moe.habib9@gmail.com>
2024-06-28 13:17:12 +0100

    tmp commit

--- ee/cli/cmd/digger/default.go
+++ ee/cli/cmd/digger/default.go
@@ -17,51 +17,73 @@ import (
 	"strconv"
 )
 
+func BackendlessOrchestrator() error {
+	ci := digger.DetectCI()
+
+	switch ci {
+	case digger.GitHub:
+		logLeader = os.Getenv("GITHUB_ACTOR")
+		github.GitHubCI(lock, policy.PolicyCheckerProviderAdvanced{}, BackendApi, ReportStrategy, github2.GithubServiceProviderAdvanced{}, comment_updater.CommentUpdaterProviderAdvanced{}, drift.DriftNotificationProviderAdvanced{})
+	case digger.None:
+		print("No CI detected.")
+		os.Exit(10)
+	}
+
+	spec := spec.Spec{
+		JobId:     "",
+		CommentId: strconv.FormatInt(commentId, 10),
+		Job:       jobSpec,
+		Reporter: spec.ReporterSpec{
+			ReportingStrategy: "comments_per_run",
+			ReporterType:      "lazy",
+		},
+		Lock: spec.LockSpec{
+			LockType: "noop",
+		},
+		Backend: spec.BackendSpec{
+			BackendType: "noop",
+		},
+		VCS: spec.VcsSpec{
+			VcsType:   "github",
+			Actor:     jobSpec.RequestedBy,
+			RepoOwner: repoOwner,
+			RepoName:  repoName,
+		},
+		Policy: spec.PolicySpec{
+			PolicyType: "noop",
+		},
+	}
+
+}
+
 var defaultCmd = &cobra.Command{
 	Use: "default",
 	Run: func(cmd *cobra.Command, args []string) {
-		var logLeader = "Unknown CI"
-		ci := digger.DetectCI()
-
-		switch ci {
-		case digger.GitHub:
-			logLeader = os.Getenv("GITHUB_ACTOR")
-
-			spec := spec.Spec{
-				JobId:     "",
-				CommentId: strconv.FormatInt(commentId, 10),
-				Job:       jobSpec,
-				Reporter: spec.ReporterSpec{
-					ReportingStrategy: "comments_per_run",
-					ReporterType:      "lazy",
-				},
-				Lock: spec.LockSpec{
-					LockType: "noop",
-				},
-				Backend: spec.BackendSpec{
-					BackendType: "noop",
-				},
-				VCS: spec.VcsSpec{
-					VcsType:   "github",
-					Actor:     jobSpec.RequestedBy,
-					RepoOwner: repoOwner,
-					RepoName:  repoName,
-				},
-				Policy: spec.PolicySpec{
-					PolicyType: "noop",
-				},
+		actor := "unknown"
+		noBackend := os.Getenv("NO_BACKEND")
+		if noBackend != "" && noBackend != "false" {
+			log.Printf("Info: running digger in backendless mode")
+			err := BackendlessOrchestrator()
+			if err != nil {
+				log.Printf("error while running: %v", err)
 			}
+			return
+		}
 
-			github.GitHubCI(lock, policy.PolicyCheckerProviderAdvanced{}, BackendApi, ReportStrategy, github2.GithubServiceProviderAdvanced{}, comment_updater.CommentUpdaterProviderAdvanced{}, drift.DriftNotificationProviderAdvanced{})
-		case digger.None:
-			print("No CI detected.")
-			os.Exit(10)
+		// assuming that digger is now running from spec based on env variable
+		spec := os.Getenv("DIGGER_RUN_SPEC")
+		if spec == "" {
+			usage.ReportErrorAndExit(actor, fmt.Sprintf("Spec argument not set"), 8)
+		}
+		actor, err := RunSpecFromString(spec)
+		if err != nil {
+			usage.ReportErrorAndExit(actor, fmt.Sprintf("error while running spec from string: %v", err), 8)
 		}
 
 		defer func() {
 			if r := recover(); r != nil {
 				log.Println(fmt.Sprintf("stacktrace from panic: \n" + string(debug.Stack())))
-				err := usage.SendLogRecord(logLeader, fmt.Sprintf("Panic occurred. %s", r))
+				err := usage.SendLogRecord(actor, fmt.Sprintf("Panic occurred. %s", r))
 				if err != nil {
 					log.Printf("Failed to send log record. %s\n", err)
 				}
--- ee/cli/cmd/digger/run_spec.go
+++ ee/cli/cmd/digger/run_spec.go
@@ -19,6 +19,26 @@ type RunSpecConfig struct {
 	Spec string `mapstructure:"spec"`
 }
 
+func RunSpecFromString(specStr string) (string, error) {
+	var spec lib_spec.Spec
+	err := json.Unmarshal([]byte(specStr), &spec)
+	if err != nil {
+		return "", fmt.Errorf("could not load spec json: %v", err)
+	}
+	err = spec2.RunSpec(
+		spec,
+		lib_spec.VCSProvider{},
+		lib_spec.JobSpecProvider{},
+		lib_spec.LockProvider{},
+		lib_spec.ReporterProvider{},
+		lib_spec.BackendApiProvider{},
+		lib_spec.PolicyProvider{},
+		lib_spec.PlanStorageProvider{},
+		comment_summary.CommentUpdaterProviderBasic{},
+	)
+	return spec.VCS.Actor, err
+}
+
 var runSpecCmd = &cobra.Command{
 	Use:   "run_spec [flags]",
 	Short: "run a spec",
@@ -26,24 +46,10 @@ var runSpecCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		var runSpecConfig RunSpecConfig
 		viperRunSpec.Unmarshal(&runSpecConfig)
-		var spec lib_spec.Spec
-		err := json.Unmarshal([]byte(runSpecConfig.Spec), &spec)
+
+		actor, err := RunSpecFromString(runSpecConfig.Spec)
 		if err != nil {
-			usage.ReportErrorAndExit("", fmt.Sprintf("could not load spec json: %v", err), 1)
-		}
-		err = spec2.RunSpec(
-			spec,
-			lib_spec.VCSProvider{},
-			lib_spec.JobSpecProvider{},
-			lib_spec.LockProvider{},
-			lib_spec.ReporterProvider{},
-			lib_spec.BackendApiProvider{},
-			lib_spec.PolicyProvider{},
-			lib_spec.PlanStorageProvider{},
-			comment_summary.CommentUpdaterProviderBasic{},
-		)
-		if err != nil {
-			usage.ReportErrorAndExit(spec.VCS.Actor, fmt.Sprintf("error running spec: %v", err), 1)
+			usage.ReportErrorAndExit(actor, fmt.Sprintf("error running spec: %v", err), 1)
 		}
 	},
 }
