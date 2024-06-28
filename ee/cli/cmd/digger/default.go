dc937b76a7c8a56de280492c24764450a25d09bd
motatoes <moe.habib9@gmail.com>
2024-06-28 12:49:46 +0100

    tmp commit

--- ee/cli/cmd/digger/default.go
+++ ee/cli/cmd/digger/default.go
@@ -9,10 +9,12 @@ import (
 	"github.com/diggerhq/digger/ee/cli/pkg/drift"
 	github2 "github.com/diggerhq/digger/ee/cli/pkg/github"
 	"github.com/diggerhq/digger/ee/cli/pkg/policy"
+	"github.com/diggerhq/digger/libs/spec"
 	"github.com/spf13/cobra"
 	"log"
 	"os"
 	"runtime/debug"
+	"strconv"
 )
 
 var defaultCmd = &cobra.Command{
@@ -24,6 +26,32 @@ var defaultCmd = &cobra.Command{
 		switch ci {
 		case digger.GitHub:
 			logLeader = os.Getenv("GITHUB_ACTOR")
+
+			spec := spec.Spec{
+				JobId:     "",
+				CommentId: strconv.FormatInt(commentId, 10),
+				Job:       jobSpec,
+				Reporter: spec.ReporterSpec{
+					ReportingStrategy: "comments_per_run",
+					ReporterType:      "lazy",
+				},
+				Lock: spec.LockSpec{
+					LockType: "noop",
+				},
+				Backend: spec.BackendSpec{
+					BackendType: "noop",
+				},
+				VCS: spec.VcsSpec{
+					VcsType:   "github",
+					Actor:     jobSpec.RequestedBy,
+					RepoOwner: repoOwner,
+					RepoName:  repoName,
+				},
+				Policy: spec.PolicySpec{
+					PolicyType: "noop",
+				},
+			}
+
 			github.GitHubCI(lock, policy.PolicyCheckerProviderAdvanced{}, BackendApi, ReportStrategy, github2.GithubServiceProviderAdvanced{}, comment_updater.CommentUpdaterProviderAdvanced{}, drift.DriftNotificationProviderAdvanced{})
 		case digger.None:
 			print("No CI detected.")
