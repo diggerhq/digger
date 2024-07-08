7468fbe63c7dd22f52caf479cd7ed7d02ab1f8e8
motatoes <moe.habib9@gmail.com>
2024-07-08 09:57:42 +0100

    avoid casting to orgservice

--- ee/backend/ci_backends/gitlab_pipeline.go
+++ ee/backend/ci_backends/gitlab_pipeline.go
@@ -9,7 +9,18 @@ import (
 )
 
 type GitlabPipelineCI struct {
-	Client *gitlab.Client
+	Client                      *gitlab.Client
+	GitlabProjectId             int
+	GitlabmergeRequestEventName string
+	GitlabCIPipelineID          string
+	GitlabCIPipelineIID         int
+	GitlabCIMergeRequestID      int
+	GitlabCIMergeRequestIID     int
+	GitlabCIProjectName         string
+	GitlabciprojectNamespace    string
+	GitlabciprojectId           int
+	GitlabciprojectNamespaceId  int
+	GitlabDiscussionId          string
 }
 
 func (gl GitlabPipelineCI) TriggerWorkflow(spec spec.Spec, runName string, vcsToken string) error {
@@ -20,15 +31,16 @@ func (gl GitlabPipelineCI) TriggerWorkflow(spec spec.Spec, runName string, vcsTo
 	vars := map[string]string{
 		"DIGGER_RUN_SPEC":          string(specBytes),
 		"GITLAB_TOKEN":             vcsToken,
-		"MERGE_REQUEST_EVENT_NAME": "",
-		"CI_PIPELINE_ID":           "",
-		"CI_PIPELINE_IID":          "",
-		"CI_MERGE_REQUEST_ID":      "",
-		"CI_MERGE_REQUEST_IID":     strconv.Itoa(*spec.Job.PullRequestNumber),
+		"MERGE_REQUEST_EVENT_NAME": gl.GitlabmergeRequestEventName,
+		"CI_PIPELINE_ID":           gl.GitlabCIPipelineID,
+		"CI_PIPELINE_IID":          strconv.Itoa(gl.GitlabCIPipelineIID),
+		"CI_MERGE_REQUEST_ID":      strconv.Itoa(gl.GitlabCIMergeRequestID),
+		"CI_MERGE_REQUEST_IID":     strconv.Itoa(gl.GitlabCIMergeRequestIID),
 		"CI_PROJECT_NAME":          spec.VCS.RepoName,
 		"CI_PROJECT_NAMESPACE":     spec.VCS.RepoFullname,
-		"CI_PROJECT_ID":            "",
-		"CI_PROJECT_NAMESPACE_ID":  "",
+		"CI_PROJECT_ID":            strconv.Itoa(gl.GitlabProjectId),
+		"CI_PROJECT_NAMESPACE_ID":  gl.GitlabciprojectNamespace,
+		"DISCUSSION_ID":            gl.GitlabDiscussionId,
 	}
 	variables := []*gitlab.PipelineVariableOptions{}
 	for k, v := range vars {
