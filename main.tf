4ca362dc6c0efea5ad16e1c9bbd5e70cdcf8b270
Mohamed Habib <moe.habib9@gmail.com>
2024-09-05 16:13:24 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-resource "null_resource" "test10" {}
+#resource "null_resource" "test10" {}
 
-variable "TEST" {
-  default = "hello"
-}
+#variable "TEST" {
+#  default = "hello"
+#}
 
-variable "SECRET" {
-  default = "shhh"
-}
+#variable "SECRET" {
+#  default = "shhh"
+#}
 
-output "test" {
-  value = var.TEST
-}
+#output "test" {
+#  value = var.TEST
+#}
 
-output "secret" {
-  value = var.SECRET
-}
+#output "secret" {
+#  value = var.SECRET
+#}
 
-output "secret2" {
-  value = var.SECRET
-}
+#output "secret2" {
+#  value = var.SECRET
+#}
 
 
 
