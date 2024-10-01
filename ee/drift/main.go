1baca9205780a5447d4fb8db49314bd5c75363e7
motatoes <moe.habib9@gmail.com>
2024-10-01 20:51:00 +0100

    send slack notifications

--- ee/drift/main.go
+++ ee/drift/main.go
@@ -76,7 +76,10 @@ func main() {
 
 	r.POST("/repos/:repo/projects/:projectName/jobs/:jobId/set-status", middleware.JobTokenAuth(), controller.SetJobStatusForProject)
 
+	r.POST("/_internal/process_notifications", middleware.WebhookAuth(), controller.ProcessAllNotifications)
+	r.POST("/_internal/send_slack_notification_for_org", middleware.WebhookAuth(), controller.SendRealSlackNotificationForOrg)
 	r.POST("/_internal/send_test_slack_notification_for_org", middleware.WebhookAuth(), controller.SendTestSlackNotificationForOrg)
+
 	r.POST("/_internal/process_drift", middleware.WebhookAuth(), controller.ProcessAllDrift)
 	r.POST("/_internal/process_drift_for_org", middleware.WebhookAuth(), controller.ProcessDriftForOrg)
 	r.POST("/_internal/trigger_drift_for_project", middleware.WebhookAuth(), controller.TriggerDriftRunForProject)
