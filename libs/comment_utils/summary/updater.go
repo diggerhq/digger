befebc6f79a6dc8737ca80d04420df6d26ddc14f
Mohamed Habib <moe.habib9@gmail.com>
2024-11-20 09:51:28 +0000

    handle summary updating when message is too long (#1833)

--- libs/comment_utils/summary/updater.go
+++ libs/comment_utils/summary/updater.go
@@ -35,7 +35,20 @@ func (b BasicCommentUpdater) UpdateComment(jobs []scheduler.SerializedJob, prNum
 		message = message + fmt.Sprintf("|%v **%v** |<a href='%v'>%v</a> | <a href='%v'>%v</a> | %v | %v | %v|\n", job.Status.ToEmoji(), jobSpec.ProjectName, *job.WorkflowRunUrl, job.Status.ToString(), prCommentUrl, jobTypeTitle, job.ResourcesCreated, job.ResourcesUpdated, job.ResourcesDeleted)
 	}
 
-	prService.EditComment(prNumber, prCommentId, message)
+	const GithubCommentMaxLength = 65536
+	if len(message) > GithubCommentMaxLength {
+		// TODO: Handle the case where message is too long by trimming
+		log.Printf("WARN: message is too long, trimming")
+		log.Printf(message)
+		const footer = "[trimmed]"
+		trimLength := len(message) - GithubCommentMaxLength + len(footer)
+		message = message[:len(message)-trimLength] + footer
+	}
+
+	err = prService.EditComment(prNumber, prCommentId, message)
+	if err != nil {
+		log.Printf("WARNING: failed to update summary comment: %v", err)
+	}
 	return nil
 }
 
