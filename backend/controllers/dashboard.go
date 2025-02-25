6f62e73c7d091b614c40978ee208f50184a48637
motatoes <moe.habib9@gmail.com>
2025-02-25 19:00:53 +0300

    add endpoints for repo details

---
+++ backend/controllers/dashboard.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"errors"
+	"github.com/diggerhq/digger/backend/middleware"
+	"github.com/diggerhq/digger/backend/models"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+func GetDashboardStatusApi(c *gin.Context) {
+	organisationId := c.GetString(middleware.ORGANISATION_ID_KEY)
+	organisationSource := c.GetString(middleware.ORGANISATION_SOURCE_KEY)
+
+	var org models.Organisation
+	err := models.DB.GormDB.Where("external_id = ? AND external_source = ?", organisationId, organisationSource).First(&org).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.String(http.StatusNotFound, "Could not find organisation: "+organisationId)
+		}
+		return
+	}
+
+	response := make(map[string]interface{})
+
+	c.JSON(http.StatusOK, response)
+}
