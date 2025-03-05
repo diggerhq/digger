f15c36b49646d8a730d4103a23ac13d1155fb029
motatoes <moe.habib9@gmail.com>
2025-03-05 22:15:29 +0300

    add additional validation

--- backend/ci_backends/github_actions.go
+++ backend/ci_backends/github_actions.go
@@ -3,6 +3,7 @@ package ci_backends
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/diggerhq/digger/backend/utils"
 	orchestrator_scheduler "github.com/diggerhq/digger/libs/scheduler"
 	"github.com/diggerhq/digger/libs/spec"
@@ -33,6 +34,11 @@ func (g GithubActionCi) TriggerWorkflow(spec spec.Spec, runName string, vcsToken
 }
 
 func (g GithubActionCi) GetWorkflowUrl(spec spec.Spec) (string, error) {
+	if spec.JobId == "" {
+		log.Printf("Cannot get workflow URL: JobId is empty")
+		return "", fmt.Errorf("job ID is required to fetch workflow URL")
+	}
+
 	_, workflowRunUrl, err := utils.GetWorkflowIdAndUrlFromDiggerJobId(g.Client, spec.VCS.RepoOwner, spec.VCS.RepoName, spec.JobId)
 	if err != nil {
 		log.Printf("Error getting workflow ID from job: %v", err)
