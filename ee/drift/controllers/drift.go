4d41bc6d4698f820a4efa553032f8b0344f5e95f
Mohamed Habib <moe.habib9@gmail.com>
2025-07-17 19:20:41 -0700

    Update ee/drift/controllers/drift.go
    Co-authored-by: bismuthdev[bot] <177057995+bismuthdev[bot]@users.noreply.github.com>

--- ee/drift/controllers/drift.go
+++ ee/drift/controllers/drift.go
@@ -204,6 +204,7 @@ func (mc MainController) ProcessAllDrift(c *gin.Context) {
 
 	for _, org := range orgs {
 		if org.DriftEnabled == false {
+			log.Printf("Skipping org: %v because DriftEnabled=false", org.ID)
 			continue
 		}
 		cron := org.DriftCronTab
