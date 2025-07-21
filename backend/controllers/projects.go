58f7b3066c269a28412386a526481d6a03652438
SaurabhYelmame <syelmame@varonis.com>
2025-07-22 01:55:58 +0530

    fix whitespace linter in backend

--- backend/controllers/projects.go
+++ backend/controllers/projects.go
@@ -1527,7 +1527,6 @@ func AutomergePRforBatchIfEnabled(gh utils.GithubClientProvider, batch *models.D
 		batch.BatchType == orchestrator_scheduler.DiggerCommandApply &&
 		batch.CoverAllImpactedProjects &&
 		automerge {
-
 		slog.Info("Conditions met for auto-merge, proceeding",
 			"batchId", batch.ID,
 			"prNumber", batch.PrNumber,
@@ -1619,7 +1618,6 @@ func DeleteOlderPRCommentsIfEnabled(gh utils.GithubClientProvider, batch *models
 		batch.BatchType == orchestrator_scheduler.DiggerCommandPlan &&
 		batch.CoverAllImpactedProjects &&
 		deleteOlderComments {
-
 		slog.Info("Conditions met for deleting prior comments, proceeding",
 			"batchId", batch.ID,
 			"prNumber", batch.PrNumber,
@@ -1707,7 +1705,6 @@ func DeleteOlderPRCommentsIfEnabled(gh utils.GithubClientProvider, batch *models
 		}
 
 		return nil
-
 	} else {
 		if batch.BatchType != orchestrator_scheduler.DiggerCommandPlan {
 			slog.Debug("Skipping deletion of prior comments - not an plan command",
