71e3c0a3f4cae34ef0536645a5ddcf831f6e3941
Mohamed Habib <moe.habib9@gmail.com>
2025-07-23 19:49:17 -0700

    set max limit on number of impacted project per action (#2049)
    * set max limit on number of impacted project per action

--- backend/config/envgetters.go
+++ backend/config/envgetters.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"github.com/spf13/cast"
+	"log/slog"
 	"os"
 )
 
@@ -8,4 +10,18 @@ func LimitByNumOfFilesChanged() bool {
 	// if this flag is set then it will fail if there are more projects impacted than the
 	// number of files changed
 	return os.Getenv("DIGGER_LIMIT_MAX_PROJECTS_TO_FILES_CHANGED") == "1"
+}
+
+func MaxImpactedProjectsPerChange() int {
+	m := os.Getenv("DIGGER_MAX_PROJECTS_PER_CHANGE")
+	if m == "" {
+		return 99999
+	} else {
+		v, err := cast.ToIntE(m)
+		if err != nil {
+			slog.Warn("unable to cast DIGGER_MAX_PROJECTS_PER_CHANGE to int, defaulting to 99999", "DIGGER_MAX_PROJECTS_PER_CHANGE", m)
+			return 99999
+		}
+		return v
+	}
 }
