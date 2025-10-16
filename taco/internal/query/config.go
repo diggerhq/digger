2312d0ff86bac39f6978d417d7af050da8515ce9
Brian Reardon <breardon2011@gmail.com>
2025-10-16 09:22:13 -0700

    update defaults

--- taco/internal/query/config.go
+++ taco/internal/query/config.go
@@ -16,8 +16,8 @@ type SQLiteConfig struct {
 	Path              string        `envconfig:"DB_PATH" default:"./data/taco.db"`// if we call it PATH at the struct level, it will pick up the terminal path 
 	Cache             string        `envconfig:"CACHE" default:"shared"`
 	BusyTimeout       time.Duration `envconfig:"BUSY_TIMEOUT" default:"5s"`
-	MaxOpenConns      int           `envconfig:"MAX_OPEN_CONNS" default:"10"`
-	MaxIdleConns      int           `envconfig:"MAX_IDLE_CONNS" default:"5"`
+	MaxOpenConns      int           `envconfig:"MAX_OPEN_CONNS" default:"25"`
+	MaxIdleConns      int           `envconfig:"MAX_IDLE_CONNS" default:"10"`
 	PragmaJournalMode string        `envconfig:"PRAGMA_JOURNAL_MODE" default:"WAL"`
 	PragmaForeignKeys string        `envconfig:"PRAGMA_FOREIGN_KEYS" default:"ON"`
 	PragmaBusyTimeout string        `envconfig:"PRAGMA_BUSY_TIMEOUT" default:"5000"`
