1c0dc9211d6d137909235e28020ba1f7bcb606de
Brian Reardon <breardon2011@gmail.com>
2025-11-03 13:34:19 -0800

    adjust routing for login

--- ui/src/router.tsx
+++ ui/src/router.tsx
@@ -1,19 +1,10 @@
 import { createRouter } from '@tanstack/react-router';
-import { routeTree } from './routeTree.gen';
-import { terraformRoute } from '@/routes/manual/terraformWellKnown';
-
-
-const existingChildren = (routeTree as any).children ?? [] // internal but fine
-
-const mixedTree = routeTree.addChildren([
-  ...existingChildren,   // keep all file-based routes
-  terraformRoute,        // add your manual route
-])  
+import { routeTree } from './routeTree.gen';  
 
 export function getRouter() {
 
   return createRouter({
-    routeTree: mixedTree,
+    routeTree: routeTree,
     scrollRestoration: true,
   });
 }
