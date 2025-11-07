1b7293cf953378847778cef0266a815e4bc0df9f
motatoes <moe.habib9@gmail.com>
2025-11-06 17:09:53 -0800

    workos org sync

--- ui/src/api/statesman_orgs.ts
+++ ui/src/api/statesman_orgs.ts
@@ -1,21 +1,21 @@
 
 
-export async function syncOrgToStatesman(orgId: string, orgName: string, displayName: string, userId: string, adminEmail: string) {
+export async function syncOrgToStatesman(orgId: string, orgName: string, displayName: string, userId: string | null, adminEmail: string | null) {
     const response = await fetch(`${process.env.STATESMAN_BACKEND_URL}/internal/api/orgs`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${process.env.STATESMAN_BACKEND_WEBHOOK_SECRET}`,
             'X-Org-ID': "",
-            'X-User-ID': userId,
-            'X-Email': adminEmail,
+            'X-User-ID': userId ?? '',
+            'X-Email': adminEmail ?? '',
         },
         body: JSON.stringify({ 
             "external_org_id": orgId,
             "name": orgName,
             "display_name": displayName,
         })
-    })  
+    })
 
     if (response.status === 409) {
         console.log("Org already exists in statesman")
@@ -23,7 +23,7 @@ export async function syncOrgToStatesman(orgId: string, orgName: string, display
     }
 
     if (!response.ok) {
-        throw new Error(`Failed to sync organization to statesman: ${response.statusText}`);
+        throw new Error(`Failed to sync organization to statesman: ${response.statusText}`, { cause: await response.text() });
     }
 
     return response.json();
