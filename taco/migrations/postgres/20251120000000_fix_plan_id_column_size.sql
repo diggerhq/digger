3301b5e29f7e02a77ca6353b19ef3d66dd220838
Brian Reardon <breardon2011@gmail.com>
2025-11-20 12:25:32 -0500

    fix length mismatch (#2463)
    * fix length mismatch
    
    * gorm change
    
    * scrub loader change

---
+++ taco/migrations/postgres/20251120000000_fix_plan_id_column_size.sql
@@ -0,0 +1,12 @@
+-- Fix plan ID columns that are too small for TFE-style IDs
+-- TFE plan IDs use format: plan-{32chars} = 37 characters total
+-- Original varchar(36) is too small and causes INSERT errors
+
+-- Fix tfe_plans.id (primary key)
+ALTER TABLE "public"."tfe_plans" 
+  ALTER COLUMN "id" TYPE varchar(50);
+
+-- Fix tfe_runs.plan_id (foreign key reference)
+ALTER TABLE "public"."tfe_runs"
+  ALTER COLUMN "plan_id" TYPE varchar(50);
+
